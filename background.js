621941b1365514f7f5e8ecac7a6b5bbe395dfcdd
Sparsh Malhotra <sparshmalhotraaa@gmail.com>
2021-06-23 21:21:46 +0530

    Added background.js

---
+++ background.js
@@ -0,0 +1,17 @@
+chrome.tabs.onActivated.addListener((tab) => {
+  chrome.tabs.get(tab.tabId, (current_tab_info) => {
+    if (/^https:\/\/www.youtube/.test(current_tab_info.url)) {
+      chrome.tabs.executeScript(null, { file: './foreground.js' }, () =>
+        console.log('i injected')
+      );
+    }
+  });
+});
+
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  if (/^https:\/\/www.youtube/.test(tab.url)) {
+    chrome.tabs.executeScript(null, { file: './foreground.js' }, () =>
+      console.log('i injected')
+    );
+  }
+});
